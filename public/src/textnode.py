826d159a288d69cac8b9af4bc84bdc30157c7854
skye-fox <105951568+skye-fox@users.noreply.github.com>
2024-06-23 14:29:47 -0500

    Add function for converting text nodes to html nodes

--- public/src/textnode.py
+++ public/src/textnode.py
@@ -1,3 +1,6 @@
+from htmlnode import LeafNode
+
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -13,3 +16,27 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode{self.text, self.text_type, self.url}"
+
+
+def text_node_to_html_node(text_node):
+    types = ["text", "bold", "italic", "code", "link", "image"]
+
+    if text_node.text_type in types:
+        if text_node.url is not None:
+            if text_node.text_type == "link":
+                return LeafNode(
+                    tag="a",
+                    value=text_node.text,
+                    props={"href": text_node.url},
+                )
+            return LeafNode(
+                tag="img",
+                value="",
+                props={"src": text_node.url, "alt": text_node.text},
+            )
+        if text_node.text_type == "text":
+            return LeafNode(value=text_node.text)
+        return LeafNode(tag=text_node.text_type, value=text_node.text)
+    raise TypeError(
+        "text node type must be one of: text, bold, italic, code, link, image"
+    )
